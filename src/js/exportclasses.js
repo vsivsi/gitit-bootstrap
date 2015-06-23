130c38eec99c053eb08f929440ccb7566e4dbc67
Vaughn Iverson <vsivsi@yahoo.com>
2015-06-22 22:22:29 -0700

    Add js to add bootstrap classes to export controls

---
+++ src/js/exportclasses.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#exportform").addClass("form-inline form-group")
+    $("#export").addClass("form-control btn btn-default btn-sm");
+    $("#exportform select").addClass("form-control input-sm")
+});
