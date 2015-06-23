ba7d489616b53a8972731d0a04c830cf3c26ad60
Vaughn Iverson <vsivsi@yahoo.com>
2015-06-22 23:36:57 -0700

    Add bootstrap classes to password reset

--- src/js/exportclasses.js
+++ src/js/exportclasses.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-    $("#exportbox").addClass("form-inline")
-    $("#exportbox select").addClass("form-control input-sm")
+    $("#exportbox").addClass("form-inline");
+    $("#exportbox select").addClass("form-control input-sm");
     $("#export").addClass("form-control btn btn-default btn-sm");
+
+    $("form#resetPassword)
+      .addClass("form-inline")
+      .children("input")
+      .addClass("form-control");
 });
