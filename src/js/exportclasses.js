d28ca2e7749c238365587aa410d53f3dfd5fb7af
Vaughn Iverson <vsivsi@yahoo.com>
2015-06-22 23:28:43 -0700

    Remove form group

--- src/js/exportclasses.js
+++ src/js/exportclasses.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#exportbox").addClass("form-inline form-group")
+    $("#exportbox").addClass("form-inline")
     $("#exportbox select").addClass("form-control input-sm")
     $("#export").addClass("form-control btn btn-default btn-sm");
 });
