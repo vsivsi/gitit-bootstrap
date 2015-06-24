9dd6066dc1103ba81651a99ee1efdc5b41b027fc
Vaughn Iverson <vsivsi@yahoo.com>
2015-06-23 19:08:12 -0700

    Put the preview in a panel

--- src/js/exportclasses.js
+++ src/js/exportclasses.js
@@ -19,5 +19,5 @@ $(document).ready(function () {
     $("form#editform #update").addClass("btn btn-primary");
     $("form#editform #cancel").addClass("btn btn-warning");
     $("form#editform #previewButton").addClass("btn btn-info");
-
+    $("#previewpane").addClass("panel panel-default")
 });
